db30f3d902ee7d2187c257a6162e4c9d6068b599
kahiga2244 <kahigakamiru@gmail.com>
2021-04-01 12:04:13 +0300

    importing models

--- Instagram/forms.py
+++ Instagram/forms.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Image,Profile,Comment
 
-
+class  NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['profile', 'likes','comments']
 
 class CommentForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
